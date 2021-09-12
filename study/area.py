cfd3a8b2d5e92c16b9b8bf2f123c29e370eef3e7
chenjin17 <1134609824@qq.com>
2021-09-12 11:33:50 +0800

    add area.py

---
+++ study/area.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+ 
+# 计算面积函数
+def area(width, height):
+    return width * height
+ 
+def print_welcome(name):
+    print("Welcome", name)
+ 
+print_welcome("Runoob")
+w = 4
+h = 5
+print("width =", w, " height =", h, " area =", area(w, h))
