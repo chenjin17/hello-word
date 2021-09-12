6254ab04679912c08764d51de70be9a5ddd018d7
chenjin17 <1134609824@qq.com>
2021-09-12 11:53:32 +0800

    import test

---
+++ study/numpy1.py
@@ -0,0 +1,9 @@
+import numpy as np
+a = np.array([2,3,4])
+b = np.array([2.0,3.0,4.0])
+c = np.array([[1.0,2.0],[3.0,4.0]])
+d = np.array([[1,2],[3,4]],dtype=complex) # 指定数据类型
+print (a, a.dtype)
+print (b, b.dtype)
+print (c, c.dtype)
+print (d, d.dtype)
