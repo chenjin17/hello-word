c85f87c02ae8937bf9cd64af17f8dc7e1835a19b
chenjin17 <1134609824@qq.com>
2021-09-12 10:29:41 +0800

    jason

---
+++ runoob.py
@@ -0,0 +1,11 @@
+#/usr/bin/python
+# coding: UTF-8 
+dict = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'}
+ 
+dict['Age'] = 8               # 更新 Age
+dict['School'] = "菜鸟教程"  # 添加信息
+ 
+ 
+print ("dict['Age']: ", dict['Age'])
+print ("dict['School']: ", dict['School'])
+
